18e967ce87b755fd47e1a509fb7c7c2caff699c9
I Kadek Sandi Yasa <sandiyasa966@gmail.com>
2025-03-08 12:20:55 +0800

    upload project 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 "# pertemuan1"  
 "## I Kadek Sandi Yasa - 2301010070" 
+"# pertemuan1"  
+"## I Kadek Sandi Yasa - 2301010070" 
---
+++ pertemuan1/src/main/java/com/mycompany/pertemuan1/Pertemuan1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //Menampilkan Tulisan di Layar
+        System.out.print("Latihan Bahasa Java");
+    }
+}
